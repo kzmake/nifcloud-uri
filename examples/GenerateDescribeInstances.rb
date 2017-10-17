43c5b91bc282f6ab60f55b010976002f04ffde7d
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-10-18 00:23:44 +0900

    endpoint修正

--- examples/GenerateDescribeInstances.rb
+++ examples/GenerateDescribeInstances.rb
@@ -1,6 +1,6 @@
 require 'nifcloud/uri'
 
-g = Nifcloud::Uri.generator(endpoint: 'https://computing.jp-west-1.api.cloud.nifty.com/api/')
+g = Nifcloud::Uri.generator(endpoint: 'https://west-1.cp.cloud.nifty.com/api/')
 
 uri = g.Action('DescribeInstances')
 p uri
