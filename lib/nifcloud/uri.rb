1b648325cb4ec6fe896646b05525138a38b592e5
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-10-17 23:04:38 +0900

    もとをちょちょいっと作成

--- lib/nifcloud/uri.rb
+++ lib/nifcloud/uri.rb
@@ -1,7 +1,24 @@
-require "nifcloud/uri/version"
+require 'nifcloud/uri/version'
+require 'nifcloud/uri/signature'
+require 'nifcloud/uri/configuration'
+require 'nifcloud/uri/generator'
 
 module Nifcloud
   module Uri
-    # Your code goes here...
+    extend Configuration
+
+    def self.generator(options={})
+      Nifcloud::Uri::Generator.new(options)
+    end
+
+    def self.method_missing(method, *args, &block)
+      return super unless generator.respond_to?(method)
+      generator.send(method, *args, &block)
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      generator.respond_to?(method_name) || super
+    end
   end
 end
+
